c2a5d3b470733e33305b3a17c7c3fda62b58e468
Caua emanuel <emanuellcaua8@gmail.com>
2025-05-19 00:33:37 -0300

    terminando pagina de Login

--- login-page/src/app/pages/login/login.component.ts
+++ login-page/src/app/pages/login/login.component.ts
@@ -1,18 +1,53 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginServiceService } from '../../services/login-service.service';
+import { ToastrService } from 'ngx-toastr';
+
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
+  providers: [
+    LoginServiceService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-  });
+
+  loginForm!: FormGroup<LoginForm>;
+
+  constructor(
+    private router: Router,
+    private LoginService: LoginServiceService,
+    private toastService: ToastrService
+
+  ){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+  submit() {
+    this.LoginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success('Login successful'),
+      error: () => this.toastService.error('Login failed')
+    })
+  }
+
+  navigate() {
+    this.router.navigate(['/signup']);
+  }
 }
